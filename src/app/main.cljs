a159f4db45de7b4c69286620ad55ed8231d4feb3
nharsch <nigelharsch@gmail.com>
2021-02-26 23:31:11 -0500

    log curve mic input

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -3,22 +3,29 @@
    [goog.dom :as d]
    ))
 
-;; (defn setup [p]
-;;   ; bind start button
-;;     (.createCanvas p 640 480)
-;;     ;; (canv.mousePressed p.userStartAudio)
-;;     (def mic (js/window.p5.AudioIn.))
-;;     (.start mic)
-;; )
 
-;; (defn draw [p]
-;;     (.background p 0)
-;;     ;; (p.text "tap to start" (/ 2 p.width) 20)
-;;     ;; (let [dark-level (- 255 (* 255 (.getLevel mic)))]
-;;     ;;     (.fill p dark-level)
-;;         ;; (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))
-;; )
+; TODO: mic input level atom
+(def app-state (atom { :mic-level 0 }))
+; TODO: FFT input atom
+;
+;
+(defn get-brush-rgb []
+  [
+   (* 255 ((comp Math/log2 inc) (:mic-level @app-state)))
+   0
+   0
+   ]
+)
 
+;; (map (comp Math/log2 inc) [0 0.25 0.5 0.75 1])
+
+
+
+
+;; @app-state
+
+;; (.getLevel mic)
+;; (get-brush-rgb)
 
 (defn setup [p]
   (def cnv (.createCanvas p 640 480))
@@ -26,14 +33,14 @@
   ;; (cnv.mousePressed (fn [] p.userStartAudio))
   (def mic (js/window.p5.AudioIn.))
   (.start mic)
-  )
+)
 
 (defn draw [p]
+  (swap! app-state assoc :mic-level (.getLevel mic))
   (.fill p 50)
   (.text p "tap to start" 300 300)
-  (let [dark-level (* 255 (.getLevel mic))]
-    (.fill p dark-level)
-    (.ellipse p (.-mouseX p) (.-mouseY p) 80 80)))
+  (.fill p (first (get-brush-rgb)))
+  (.ellipse p (.-mouseX p) (.-mouseY p) 80 80))
 
 (def parent-id  "example")
 (when-not (d/getElement parent-id)
@@ -45,5 +52,3 @@
       (set! (.-setup p) (fn [] (setup p)))
       (set! (.-draw p) (fn [] (draw p))))
        parent-id))
-
-

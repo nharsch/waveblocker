20adc9ad2ef5f7243cd8a31f65602dc0892b65df
nharsch <nigelharsch@gmail.com>
2021-03-12 01:04:52 -0500

    stub out volume sensitivity

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -16,12 +16,9 @@
          :pitch 0
          :history []
          :redo []
-         }
-  )
-)
+         }))
 ; drawing separate atom state for performance?
 (def drawing (atom false))
-
 (defn undo []
   (swap! app-state assoc :redo (conj (:redo @app-state) (last (:history @app-state))))
   (swap! app-state assoc :history (pop (:history @app-state)))
@@ -33,17 +30,17 @@
   (swap! app-state assoc :history (conj (:history @app-state) (last (:redo @app-state))))
   (swap! app-state assoc :redo (pop (:redo @app-state)))
   (println "redo")
-  (println @app-state)
-  )
+  (println @app-state))
 
+(defn turn-on [] (swap! app-state assoc :app-on true))
+
+; utils
 (defn v-update-last [v f]
   "return vector with last element updated"
   (conj
    (into [] (butlast v))
    (f (last v))))
 ;; (v-update-last [[{:x 1}{:x 2}] [{:x 1}{:x 2}]] (fn [v] (conj v {:x 3})))
-
-(defn turn-on [] (swap! app-state assoc :app-on true))
 
 (defn geo-sequence [a r n]
   (* a (Math/pow r (- n 1))))
@@ -56,6 +53,32 @@
      1))
 ;; (= (inverse-geo-prog 1 2 4) 3)
 ;; (= (inverse-geo-prog 110 2 880) 4)
+;;
+;;
+(defn level-curve [lmin lmax level]
+  (Math/log2
+   (+ 1
+  ;;     ; set max vol ~ 80% min vol ~10%
+      (*
+       (/ 1 (- lmax lmin))
+       (-
+        (max lmin (min lmax level))
+        lmin)
+       ))))
+
+
+(map (partial level-curve 0 0.1) [0 0.5 1])
+
+(def level-curve-memo (memoize level-curve))
+
+(defn get-mic-level [mic]
+  ; curve val
+  ;; (level-curve-memo (.getLevel mic))
+  (level-curve 0 0.2 (.getLevel mic)))
+
+
+
+;; (map Math/log2 [1 1.5 2])
 
 (defn find-base-oct-freq [base freq]
   "given a minimum base frequency, find the nearest octave belor freq"
@@ -67,7 +90,7 @@
         base
         (find-base-oct-freq high freq)))))
 
-
+; TODO memoize
 (defn freq-to-hue [base-freq color-max freq]
   (let [low (find-base-oct-freq base-freq freq)
         i (* (- freq low) (/ 1 low))]
@@ -77,6 +100,7 @@
 ;; (map (partial freq-to-hue 110 100) [220 440 880])
 
 
+; TODO: memoize
 (defn freq-to-saturation [base octave-range sat-range freq]
   "given a base frequency, range of octaves, and a color range (from 0), find saturation value for frequency"
   (let [high (geo-sequence base 2 (+ 1 octave-range))]
@@ -117,7 +141,7 @@
      :y (.-mouseY p)
      :h (freq-to-hue BASE 100 freq)
      :s (freq-to-saturation BASE OCTAVE-RANGE 100 freq)
-     :b (+ 10 (* 100 (.getLevel mic)))}))
+     :b (+ 10 (* 100 (get-mic-level mic)))}))
 
 
 (defn draw [p]
@@ -164,10 +188,20 @@
             (swap! app-state assoc :history nh)))
         (.colorMode p "hsb" 100)
         (.noStroke p)
-; draw paint tip
+        ; draw paint tip
         (let [m (paint-map p)]
           (.fill p (:h m) (:s m) (:b m))
-          (.ellipse p (:x m) (:y m) 60 60)))))
+          (.ellipse p (:x m) (:y m) 60 60)
+          ; level fader
+          (.rect p 0 10 (* 250 (get-mic-level mic)) 10))
+        (.fill p  0 0 50)
+        ; level ref
+        (.rect p 0 0 250 10)
+        (.fill p 0 0 0)
+        (.text p "min vol" 1 8)
+        (.fill p 0 0 100)
+        (.text p "max vol" 205 8)
+        )))
 
 (gevents/listen (d/getElement "undo") (.-CLICK gevents/EventType) (fn [e] (undo)))
 (gevents/listen (d/getElement "redo") (.-CLICK gevents/EventType) (fn [e] (redo)))
